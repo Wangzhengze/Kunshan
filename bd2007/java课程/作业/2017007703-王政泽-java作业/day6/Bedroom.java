22859582e7ac209f01172bb4d79eb036568c1fd7
Wangzhengze <944142041@qq.com>
2020-08-20 14:11:28 +0800

    添加讲义

--- bd2007/java课程/作业/2017007703-王政泽-java作业/day6/Bedroom.java
+++
@@ -1,7 +0,0 @@
-package com.briup.day06;
-
-public class Bedroom {
-	public Windows[] window = new Windows[2];
-	public Door door;
-
-}
--- bd2007/java课程/作业/2017007703-王政泽-java作业/线程答案(8)/Thread1.java
+++
@@ -1,9 +0,0 @@
-package com.briup.day5;
-
-public class Thread1 implements Runnable {
-
-    @Override
-    public void run() {
-
-    }
-}
